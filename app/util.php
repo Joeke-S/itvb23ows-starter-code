c891e777c14a10e61e1d75987dcfe86607869fcf
Joeke-S <joeke.schaafsma@ziggo.nl>
2024-02-01 13:35:48 +0100

    Toch maar niet alles OOP. Even terug naar af...

--- app/util.php
+++ app/util.php
@@ -2,32 +2,30 @@
 
 $GLOBALS['OFFSETS'] = [[0, 1], [0, -1], [1, 0], [-1, 0], [-1, 1], [1, -1]];
 
-function isNeighbour($a, $b): bool
-{
+function isNeighbour($a, $b) {
     $a = explode(',', $a);
     $b = explode(',', $b);
-
-    if (($a[1] == $b[1] && abs($a[0] - $b[0]) == 1) ||
-        ($a[0] + $a[1] == $b[0] + $b[1]) ||
-        ($a[0] == $b[0] && abs($a[1] - $b[1]) == 1)) {
+    if ($a[0] == $b[0] && abs($a[1] - $b[1]) == 1) {
         return true;
     }
-
+    if ($a[1] == $b[1] && abs($a[0] - $b[0]) == 1) {
+        return true;
+    }
+    if ($a[0] + $a[1] == $b[0] + $b[1]) {
+        return true;
+    }
     return false;
 }
 
-function hasNeighbour($a, $board): bool
-{
+function hasNeighBour($a, $board) {
     foreach (array_keys($board) as $b) {
         if (isNeighbour($a, $b)) {
             return true;
         }
     }
-    return false;
 }
 
-function neighboursAreSameColor($player, $a, $board): bool
-{
+function neighboursAreSameColor($player, $a, $board) {
     foreach ($board as $b => $st) {
         if (!$st) {
             continue;
@@ -40,23 +38,23 @@ function neighboursAreSameColor($player, $a, $board): bool
     return true;
 }
 
-function len($tile)
-{
+function len($tile) {
     return $tile ? count($tile) : 0;
 }
 
-function slide($board, $from, $to)
-{
-    if ((!hasNeighBour($to, $board)) || (!isNeighbour($from, $to))) {
+function slide($board, $from, $to) {
+    if (!hasNeighBour($to, $board)) {
         return false;
     }
-
+    if (!isNeighbour($from, $to)) {
+        return false;
+    }
     $b = explode(',', $to);
     $common = [];
     foreach ($GLOBALS['OFFSETS'] as $pq) {
         $p = $b[0] + $pq[0];
         $q = $b[1] + $pq[1];
-        if (isNeighbour($from, $p . "," . $q)) {
+        if (isNeighbour($from, $p.",".$q)) {
             $common[] = $p . "," . $q;
         }
     }
@@ -65,3 +63,4 @@ function slide($board, $from, $to)
     }
     return min(len($board[$common[0]]), len($board[$common[1]])) <= max(len($board[$from]), len($board[$to]));
 }
+
