4a17398d845b6bdde5233b9c2508177ff3c06af3
Joeke-S <joeke.schaafsma@ziggo.nl>
2024-03-15 16:59:37 +0100

    Start Test functions

---
+++ app/tests/GamePrinterTest.php
@@ -0,0 +1,40 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class GamePrinterTest extends TestCase
+{
+
+    public function printMoveHistory()
+    {
+
+    }
+
+    public function printHand()
+    {
+
+    }
+    public function printBoard()
+    {
+
+    }
+
+    public function printMoveTo()
+    {
+
+    }
+    public function printMoveFrom()
+    {
+
+    }
+
+    public function printPlayTo()
+    {
+
+    }
+
+    public function printPiecesAvailable()
+    {
+
+    }
+
+}
--- app/tests/HiveGameTest.php
+++ app/tests/HiveGameTest.php
@@ -1,7 +1,89 @@
 <?php
 use PHPUnit\Framework\TestCase;
+use HiveGame;
 
 final class HiveGameTest extends TestCase
 {
 
+    public function testCreateGame()
+    {
+
+    }
+    public function testSetPieceMovesTo()
+    {
+
+    }
+
+    public function testSetState()
+    {
+
+    }
+    public function testGetHand()
+    {
+        $game = new HiveGame();
+
+        $this->assertSame(
+            [
+                0 =>
+                    ["Q" => 1, "B" => 2, "S" => 2, "A" => 3, "G" => 3],
+                1 =>
+                    ["Q" => 1, "B" => 2, "S" => 2, "A" => 3, "G" => 3]
+            ],
+            $game->getHand()
+        );
+
+    }
+
+    public function testGetBoard()
+    {
+        $game = new HiveGame();
+
+        $this->assertSame([], $game->getBoard());
+
+    }
+
+    public function testGetPlayer()
+    {
+        $game = new HiveGame();
+
+        $this->assertSame([], $game->getPlayer());
+    }
+    public function testGetGameId()
+    {
+        $game = new HiveGame();
+
+        $this->assertSame([], $game->getGameId());
+    }
+    public function testGetMovesTo()
+    {
+
+    }
+    public function testGetHandPlayer()
+    {
+
+    }
+    public function testMove()
+    {
+
+    }
+
+    public function testPass()
+    {
+
+    }
+
+    public function testPlay()
+    {
+
+    }
+
+    public function testRestart()
+    {
+
+    }
+
+    public function testUndo()
+    {
+
+    }
 }
